07debec92f649bfe1cab315426844283a1f4f0fc
Lucas <lucas@ecoding.com.br>
2020-07-27 20:14:04 -0300

    change footer logo and text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import Logo from '../../assets/img/Logo.png'
 import { FooterBase } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
-      <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
-      </p>
+      <img className='Logo' src={Logo} alt='PittaFlix' />
+      <p> Orgulhosamente criado durante a Imersão React da Alura </p>
     </FooterBase>
   );
 }
