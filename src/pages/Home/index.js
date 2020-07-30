31c1720525426b8826e6de6a2bd9ded8fb31af9c
Lucas <lucas@ecoding.com.br>
2020-07-29 23:02:53 -0300

    remove google with btn component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,6 @@ import Menu from '../../components/Menu'
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
 
 function Home() {
   return (
@@ -40,8 +39,6 @@ function Home() {
       <Carousel
         category={dadosIniciais.categorias[5]}
       />      
-
-      <Footer />
     </div>
   );
 }
